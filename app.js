9489a7fdda9b22f3c9d737758a53d8a064be629d
Dave Fisher <dave.j.fish@gmail.com>
2022-06-03 11:03:31 -0700

    updated greeting card to be in three sections, updated themes to change sections with switch funciton

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const cardImage = document.getElementById('cardImage');
 const userTheme = document.getElementById('theme');
 const greetingCard = document.getElementById('card');
 
+const sectionOne = document.getElementById('sectionOne');
+const sectionTwo = document.getElementById('sectionTwo');
+const sectionThree = document.getElementById('sectionThree');
+
 const exportImage = document.getElementById('exportImage');
 
 
@@ -29,8 +33,42 @@ pictureSelect.addEventListener('change', () => {
 });
 
 userTheme.addEventListener('change', () => {
-    greetingCard.classList.value = 'greetingCard';
-    greetingCard.classList.add(userTheme.value);
+    switch (userTheme.value) {
+        case 'forest':
+            sectionOne.classList.value = 'sectionOne';
+            sectionOne.classList.add('forest');
+            sectionTwo.classList.value = 'sectionTwo';
+            sectionTwo.classList.add('forestTwo');
+            sectionThree.classList.value = 'sectionThree';
+            sectionThree.classList.add('forest');
+            break;
+        case 'river':
+            sectionOne.classList.value = 'sectionOne';
+            sectionOne.classList.add('river');
+            sectionTwo.classList.value = 'sectionTwo';
+            sectionTwo.classList.add('riverTwo');
+            sectionThree.classList.value = 'sectionThree';
+            sectionThree.classList.add('river');
+            greetingCard.classList.value = 'greetingCard';
+            greetingCard.classList.add('dashedBorder');
+            break;
+        case 'mountain':
+            sectionOne.classList.value = 'sectionOne';
+            sectionOne.classList.add('mountain');
+            sectionTwo.classList.value = 'sectionTwo';
+            sectionTwo.classList.add('mountainTwo');
+            sectionThree.classList.value = 'sectionThree';
+            sectionThree.classList.add('mountain');
+            break;
+        case 'game':
+            sectionOne.classList.value = 'sectionOne';
+            sectionOne.classList.add('game');
+            sectionTwo.classList.value = 'sectionTwo';
+            sectionTwo.classList.add('gameTwo');
+            sectionThree.classList.value = 'sectionThree';
+            sectionThree.classList.add('game');
+            break;
+    }
 });
 
 exportImage.addEventListener('click', async() => {

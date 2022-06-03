2c2ed49670314bde5c3690919461b5d0fbe20134
Dave Fisher <dave.j.fish@gmail.com>
2022-06-02 17:12:12 -0700

    heading input updates card greeting card text

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // grab DOM elements
 
+const heading = document.getElementById('heading');
+const cardHeading = document.getElementById('userText');
+
+
+
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+
+heading.addEventListener('input', () => {
+    cardHeading.textContent = heading.value;
+});
